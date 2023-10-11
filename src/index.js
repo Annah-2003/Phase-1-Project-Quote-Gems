0714037307bd09bbc862ab1359cd92e174fe49fe
Irene Gitau <irene.gitau@strathmore.edu>
2023-10-11 16:57:25 +0300

    done

--- src/index.js
+++ src/index.js
@@ -16,3 +16,18 @@ function getQuote() {
             console.error("Error fetching quote:", error);
         });
 }
+function get_quote_of_the_day() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+	 if (this.readyState == 4 && this.status == 200) {
+	     // Access the result here
+	     alert(this.responseText);
+	 }
+    };
+    xhttp.open("GET", "https://quotes.rest/qod?category=inspire", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.setRequestHeader("X-Theysaidso-Api-Secret", "YOUR API HERE");
+    xhttp.send();
+}
+
+get_quote_of_the_day()
